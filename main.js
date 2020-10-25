363cc6b7748739240c2eac0298e4d46b6adc8bd3
TheAdvancingDev <theadvancingdev@gmail.com>
2020-10-25 16:22:52 -0400

    updated urls links in main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const logoGlow = document.getElementById('logo-glow');
 
 window.setInterval(() => {
-    logoGlow.style.background = 'url("../images/di-logo-4-glow.svg")'
+    logoGlow.style.background = 'url("./images/di-logo-4-glow.svg")'
 
     window.setTimeout(() => {
-        logoGlow.style.background = 'url("../images/di-logo-4.svg")';
+        logoGlow.style.background = 'url("./images/di-logo-4.svg")';
     }, 2500)
 
 }, 5000);
